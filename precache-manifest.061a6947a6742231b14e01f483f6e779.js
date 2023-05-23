8132572c2ba3c74c3931a2034e55aa64defdd529
isaitejan <isaitejan@gmail.com>
2023-05-23 11:56:57 -0500

    Updates

---
+++ precache-manifest.061a6947a6742231b14e01f483f6e779.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "286be8626c7c4cf87a07",
+    "url": "/Frontend-Code-Editor/static/css/main.8e5738be.chunk.css"
+  },
+  {
+    "revision": "286be8626c7c4cf87a07",
+    "url": "/Frontend-Code-Editor/static/js/main.286be862.chunk.js"
+  },
+  {
+    "revision": "98e85b965ef2a1ef56da",
+    "url": "/Frontend-Code-Editor/static/css/1.43bdaf8d.chunk.css"
+  },
+  {
+    "revision": "98e85b965ef2a1ef56da",
+    "url": "/Frontend-Code-Editor/static/js/1.98e85b96.chunk.js"
+  },
+  {
+    "revision": "3423e5020b2e2d621b1d",
+    "url": "/Frontend-Code-Editor/static/js/runtime~main.3423e502.js"
+  },
+  {
+    "revision": "c31e449baffc1cb77b69f7129b8ec737",
+    "url": "/Frontend-Code-Editor/index.html"
+  }
+];
